92c0cd92bbf19e4a45764d3ca7f9a34cdaf4fa7b
lindsay <lindsay333@gmail.com>
2019-03-10 16:22:08 -0400

    Added data types file

---
+++ data_types.py
@@ -0,0 +1,30 @@
+'''
+Overview of python data types
+'''
+
+# this is a string
+boardgame = "castles of burgundy"
+
+# concatenation example
+str1 = "ab"
+str2 = "cd"
+str3 = str1 + str2 # str3 = "abcd"
+
+# this is a boolean
+keith_always_wins = False
+
+# this is an integer
+num_of_plays = 5
+keith_win_count = 2
+
+# this is a float?
+keith_win_percentage = keith_win_count / num_of_plays
+
+# this is explicitly a float
+this_float = 0.0
+
+print("boardgame: ", boardgame)
+print("keith always wins: ", keith_always_wins)
+print("number of plays: ", num_of_plays)
+print("keith win count: ", keith_win_count)
+print("keith_win_percentage: ", keith_win_percentage)
